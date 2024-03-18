aa682b497ab83043c9e22bde7fc44a28b6c07b42
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-03-18 19:30:12 +0530

    Update commands.py

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -65,7 +65,8 @@ async def start(b, m):
     await m.reply_text(
     #chat_id=m.chat.id,
     #photo="https://telegra.ph/file/d813fe75a3ac675ef34b7.jpg",
-    text= SRT_TXT.format(m.from_user.mention, disable_web_page_preview=True),
+    text= SRT_TXT.format(m.from_user.mention), 
+    disable_web_page_preview=True,
     reply_markup=InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🤡", url="https://t.me/m_house786")],
